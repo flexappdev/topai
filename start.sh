369f13c9a4775a46930bcda407bbf83510570cb9
Mat Siems <mat@matsiems.com>
2025-08-28 16:22:04 +0100

    feat: add docker login from secrets

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+# Log in to Docker Hub using credentials from top-level secrets if available
+if [ -f "/run/secrets/DOCKER_USERNAME" ] && [ -f "/run/secrets/DOCKER_PASSWORD" ]; then
+  DOCKER_USERNAME=$(cat /run/secrets/DOCKER_USERNAME)
+  DOCKER_PASSWORD=$(cat /run/secrets/DOCKER_PASSWORD)
+fi
+
+if [ -n "$DOCKER_USERNAME" ] && [ -n "$DOCKER_PASSWORD" ]; then
+  echo "$DOCKER_PASSWORD" | sudo docker login -u "$DOCKER_USERNAME" --password-stdin >/dev/null 2>&1
+fi
+
 sudo docker-compose down --volumes --remove-orphans
 sudo docker-compose build --no-cache
 sudo docker-compose up -d
